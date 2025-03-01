9a363970cb34ea9b62d23a51d73f899735433113
jrc03c <jrc03c@pm.me>
2025-03-01 10:01:26 -0600

    ...

--- demo/bundle.js
+++ demo/bundle.js
@@ -21470,19 +21470,21 @@ ${JSON.stringify(newTargetLocation, null, 2)}
     /* html */
     `
   <div id="banner-form-container">
-    <form>
-      <select>
-        <option value="">(none)</option>
-        <option value="dark">dark</option>
-        <option value="danger">danger</option>
-        <option value="warning">warning</option>
-        <option value="success">success</option>
-        <option value="primary">primary</option>
-        <option value="info">info</option>
-        <option value="link">link</option>
-      </select>
+    <form @submit.prevent="onSubmit">
+      <div class="row row-left row-tight">
+        <select v-model="color">
+          <option value="">(none)</option>
+          <option value="dark">dark</option>
+          <option value="danger">danger</option>
+          <option value="warning">warning</option>
+          <option value="success">success</option>
+          <option value="primary">primary</option>
+          <option value="info">info</option>
+          <option value="link">link</option>
+        </select>
 
-      <input type="submit" value="Show banner" />
+        <input type="submit" value="Show banner" />
+      </div>
     </form>
   </div>
 `
@@ -21492,8 +21494,43 @@ ${JSON.stringify(newTargetLocation, null, 2)}
     template,
     data() {
       return {
+        color: "info",
         css
       };
+    },
+    methods: {
+      onSubmit() {
+        this.removeAllBanners();
+        const banner = document.createElement("div");
+        document.body.appendChild(banner);
+        banner.classList.add("banner");
+        banner.classList.add("bottom");
+        banner.innerHTML = `
+        <div>Accept these cookies \u2014 or else!</div>
+        <button>Okay...</button>
+      `;
+        const button = banner.querySelector("button");
+        if (this.color) {
+          banner.classList.add(this.color);
+          button.classList.add(this.color);
+        }
+        button.addEventListener("click", () => {
+          if (banner.parentElement) {
+            banner.parentElement.removeChild(banner);
+          }
+        });
+      },
+      removeAllBanners() {
+        const existingBanners = Array.from(document.querySelectorAll(".banner"));
+        existingBanners.forEach((b) => {
+          if (b.parentElement) {
+            b.parentElement.removeChild(b);
+          }
+        });
+      }
+    },
+    unmounted() {
+      this.removeAllBanners();
     }
   });
 
--- demo/src/views/banner.mjs
+++ demo/src/views/banner.mjs
@@ -10,19 +10,21 @@ const css = /* css */ ``
 
 const template = /* html */ `
   <div id="banner-form-container">
-    <form>
-      <select>
-        <option value="">(none)</option>
-        <option value="dark">dark</option>
-        <option value="danger">danger</option>
-        <option value="warning">warning</option>
-        <option value="success">success</option>
-        <option value="primary">primary</option>
-        <option value="info">info</option>
-        <option value="link">link</option>
-      </select>
+    <form @submit.prevent="onSubmit">
+      <div class="row row-left row-tight">
+        <select v-model="color">
+          <option value="">(none)</option>
+          <option value="dark">dark</option>
+          <option value="danger">danger</option>
+          <option value="warning">warning</option>
+          <option value="success">success</option>
+          <option value="primary">primary</option>
+          <option value="info">info</option>
+          <option value="link">link</option>
+        </select>
 
-      <input type="submit" value="Show banner" />
+        <input type="submit" value="Show banner" />
+      </div>
     </form>
   </div>
 `
@@ -39,51 +41,55 @@ const BannerView = createVueComponentWithCSS({
 
   data() {
     return {
+      color: "info",
       css,
     }
+  },
+
+  methods: {
+    onSubmit() {
+      this.removeAllBanners()
+
+      const banner = document.createElement("div")
+      document.body.appendChild(banner)
+      banner.classList.add("banner")
+      banner.classList.add("bottom")
+
+      banner.innerHTML = `
+        <div>Accept these cookies — or else!</div>
+        <button>Okay...</button>
+      `
+
+      const button = banner.querySelector("button")
+
+      if (this.color) {
+        banner.classList.add(this.color)
+        button.classList.add(this.color)
+      }
+
+      button.addEventListener("click", () => {
+        if (banner.parentElement) {
+          banner.parentElement.removeChild(banner)
+        }
+      })
+    },
+
+    removeAllBanners() {
+      const existingBanners = Array.from(document.querySelectorAll(".banner"))
+
+      existingBanners.forEach(b => {
+        if (b.parentElement) {
+          b.parentElement.removeChild(b)
+        }
+      })
+    },
+  },
+
+  unmounted() {
+    this.removeAllBanners()
   },
 })
 
 export { BannerView }
 
-// !(() => {
-//   const container = document.querySelector("#banner-form-container")
-//   const form = container.querySelector("form")
-
-//   form.addEventListener("submit", event => {
-//     event.preventDefault()
-//     event.stopImmediatePropagation()
-
-//     const existingBanners = Array.from(document.querySelectorAll(".banner"))
-
-//     existingBanners.forEach(b => {
-//       if (b.parentElement) {
-//         b.parentElement.removeChild(b)
-//       }
-//     })
-
-//     const color = form.querySelector("select").value
-//     const banner = document.createElement("div")
-//     document.body.appendChild(banner)
-//     banner.classList.add("banner")
-//     banner.classList.add("bottom")
-
-//     banner.innerHTML = `
-//     <div>Accept these cookies — or else!</div>
-//     <button>Okay...</button>
-//   `
-
-//     const button = banner.querySelector("button")
-
-//     if (color) {
-//       banner.classList.add(color)
-//       button.classList.add(color)
-//     }
-
-//     button.addEventListener("click", () => {
-//       if (banner.parentElement) {
-//         banner.parentElement.removeChild(banner)
-//       }
-//     })
-//   })
-// })()
+!(() => {})()
